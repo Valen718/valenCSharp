429d6fa56c94c0373e7097bcd8f56e8e793ef591
Valentin Suarez <164792863+Valen718@users.noreply.github.com>
2025-04-22 11:40:53 -0300

    Add files via upload
    Este es un examen de 2024 que no pude hacer en ese momento, actualmente 2025 puedo resolverlo.

---
+++ Medico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_C_
+{
+    class Medico : Persona
+    {
+        private enum _especializacion
+        {
+            cirujano,
+            cardiologo
+        }
+        private _especializacion especializacion;
+
+        public Medico(string nombre, string dni) : base(nombre, dni)
+        {
+            especializacion = _especializacion.cardiologo;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre del medico: {nombre}, dni: {dni}, especialidad: {especializacion}";
+        }
+    }
+}
